378cea1c1eed7de83d48935f1375f8dd7201c25a
Robin Malfait <malfait.robin@gmail.com>
2021-02-27 20:34:17 +0100

    fix potential memory leaks

--- src/utils/create-error.ts
+++ src/utils/create-error.ts
@@ -1,6 +1,11 @@
 export function createError<T>(message: string, attributes?: T) {
   let error = Object.assign(new Error(message), attributes)
 
+  // In V8, Error objects keep the closure scope chain alive until the
+  // err.stack property is accessed.
+  // eslint-disable-next-line
+  error.stack
+
   // This allows us to remove this createError function from the stack trace.
   // This means that your stack trace will be useful and point to the place
   // where the error is possibly thrown and _not_ to this function!
